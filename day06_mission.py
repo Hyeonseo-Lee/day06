869abd2e6db5fa93e069430ab59e04b6f9ff02ae
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-19 18:13:17 +0900

    9_4

--- day06_mission.py
+++ day06_mission.py
@@ -1,14 +1,9 @@
-#9.3
+#9.4
 
-def test(func):
-    def new_test(*args, **kwargs):
-        print('start')
-        result = func(*args, **kwargs)
-        print(result)
-        return('end')
-    return new_test
+class OopsException(Exception):
+    pass
 
-def sum(a, b):
-    return (a + b)
-
-print(test(sum)(1, 2))
+try:
+    raise OopsException('Caught an oops')
+except OopsException as err:
+    print(OopsException)
