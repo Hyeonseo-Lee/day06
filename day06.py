63585abf1bc95a560d0372fa6dd6789c0d4b0bac
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-19 13:10:59 +0900

    factorial

--- day06.py
+++ day06.py
@@ -1,17 +1,24 @@
-g = 1 #global variable
+def factorial_iter(n):
+    """
+    반복문을 사용한 팩토리얼 함수
+    :param n: n!
+    :return: 팩토리얼 계산 결과 값
+    """
+    result = 1
+    for k in range(1, n+1):
+        result = result * k
+    return result
 
-def print_global():
-    #g = 1 #local variable
-    print(g)
+def factorial_recu(n):
+    """
+    재귀 함수를 사용한 팩토리얼 계산 함수
+    :param n:
+    :return: 자기 자신을 호출 또는 1
+    """
+    if n == 1: #끝나는 조건
+        return 1
+    else:
+        return factorial_recu(n-1) * n
 
-def change_print_global():
-    global g
-    print(g)
-    g = 2
-    print(g)
-
-change_print_global()
-print(g)
-
-print(globals())
-
+print(factorial_recu(50))
+print(factorial_iter(50))
