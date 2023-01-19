dd15bdd802965c2db97a4c56e784946503f76e20
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-19 14:20:17 +0900

    chapter9_exercise

--- day06.py
+++ day06.py
@@ -1,28 +1,23 @@
-try:
-    raise typeerror('타입에러')
-    expr = input('분자 분모 입력 (띄어쓰기로 구분) : ').split()
-    print(int(expr[0]) / int(expr[1]))
-except ValueError as err:
-    print(f'숫자를 입력해주세요 ({err})')
-except ZeroDivisionError as err:
-    print(err)
-    print(f'분모에 0이 올 수 없습니다({err})')
-except IndexError as err:
-    print('입력 값의 범위에 문제가 있습니다 ({err})')
-except Exception as other:
-    print('예외 발생! : {other}')
-else: #예외가 발생 하지 않았을 때
-    print('프로그램 정상', end=' ')
-finally: #예외 발생 여부와 상관 없이 무조건 실행
-    print('종료')
+#ex 9-1
+#
+# def good():
+#     return ['Harry', 'Ron', 'Hermione']
 
-# def div_calc(a, b):
-#     """
-#     나누기 함수
-#     :param a: 분자
-#     :param b: 분모
-#     :return: 계산결과
-#     """
-#     return a/b
-# print(div_calc(15, 3))
-# print(div_calc(7, 0))
+# ranger = (number for number in range(10) if number % 2 == 1)
+#
+# for x in ranger:
+#     print(x)
+
+#ex 9-2
+cnts = 0
+def get_odds():
+    for number in range(10):
+        if number % 2:
+            yield number
+
+odds = get_odds()
+for i in odds:
+    cnts = cnts + 1
+    if cnts == 3:
+        print(i)
+
