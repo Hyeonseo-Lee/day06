0a5f1779bd7d5af237d2ca6f81aa91266ebab5b7
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-19 10:40:59 +0900

    generator_1

--- day06.py
+++ day06.py
@@ -1,13 +1,22 @@
-def a():     #generator
-    yield 1
-    yield 2
-    yield 3
+# decorator
+def document_info(func):
+    def new_function(*args, **kwargs):
+        print('실행 중인 함수:', func.__name__)
+        print('위치 기반 인수들:', args)
+        print('키워드 기반 인수들:', kwargs)
+        result = func(*args, **kwargs)
+        print('실행 결과:', result)
+        return result
+    return new_function
 
-def b():    #normal function
-    return 1   #stop
-    return 2
-    return 3
+@document_info
+def sub_int(x, y):
+    return x - y
 
-print(a(), b())
-for i in a():
-    print(i)
+@document_info
+def squares(n):
+    return n * n
+
+print(sub_int(7,3))
+print(squares(5))
+
