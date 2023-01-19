7e71f049300c7f8be99e8abf0609439b0baa4925
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-19 15:47:55 +0900

    inheritance

--- day06.py
+++ day06.py
@@ -12,6 +12,12 @@ class Pokemon:
         for skill in self.skills:
             print(skill)
 
+class Pikachu(Pokemon): #inheritance
+    pass
+
+pi1 = Pikachu('피카츄', '덴트', '번개')
+pi1.info()
+
 p1 = Pokemon('피카츄', '한지우', '50만 볼트/100만 볼트/번개')
 p2 = Pokemon('꼬부기', '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
 
